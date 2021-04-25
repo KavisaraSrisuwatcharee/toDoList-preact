17104ac15ca12803e2c4a77b195ac698562a8117
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-04-25 20:07:23 +0700

    [UPDATE] set state dialog

--- myTodo/src/components/EditForm/editForm.js
+++ myTodo/src/components/EditForm/editForm.js
@@ -5,34 +5,40 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import style from './editform.css'
 
 const editForm = (props) => {
-//   const [open, setOpen] = React.useState(false);
+  //   const [open, setOpen] = React.useState(false);
 
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
+  //   const handleClickOpen = () => {
+  //     setOpen(true);
+  //   };
 
-//   const handleClose = () => {
-//     setOpen(false);
-//   };
+  //   const handleClose = () => {
+  //     setOpen(false);
+  //   };
 
   return (
-    <div>
+    <div class={style.container}>
       {/* <Button variant="outlined" color="primary" onClick={handleClickOpen}>
         Open form dialog
       </Button> */}
-      <Dialog open={props.open} onClose={props.handleClose} aria-labelledby="form-dialog-title">
-        
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-        
+      <Dialog open={props.open} onClose={props.handleClose} class={style.dialog} >
+      <DialogContentText style={{ width: '300px', height: '150px'}}>
+							<div class={style.layout}>
+        <TextField style={{marginTop:'20px'}}
+          autoFocus
+          // margin="dense"
+          label="Change my todo"
+          type="email"
+          
+        />
+        <div class={style.btn}>
+          <Button onClick={props.handleClose}>change</Button>
+          <Button onClick={props.handleClose}>cancel</Button>
+        </div>
+        </div>
+        </DialogContentText>
       </Dialog>
     </div>
   );
