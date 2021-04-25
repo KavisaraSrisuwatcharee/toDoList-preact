0a337cd70b250687aed0ec36594c7c2ce67383d6
Chingly <kavisara.noya@mail.kmutt.ac.th>
2021-04-25 23:56:48 +0700

    [ADD] axios post in todoForm

--- myTodo/src/components/todoForm/todoForm.js
+++ myTodo/src/components/todoForm/todoForm.js
@@ -2,13 +2,29 @@ import { h } from 'preact';
 import { useState } from 'preact/hooks';
 import style from './todoForm.module.css'
 import ToDoList from '../todolist/todolist.js'
+import axios from "Axios"
 const toDoForm = () => {
     const [todo, setTodo] = useState([])
     const [text, setText] = useState('')
+    
+    const insertTodo = async () => {
+     const res =await axios.post('http://localhost:8080/api', data, {
+
+     })   
+    }
+    const fetchTodo = async () => {
+        const res = await axios.get('http://localhost:8080/api')
+        setTodo(res.data)
+        console.log(res.data)
+    }
     const addTodo = () => {
         setTodo([...todo, text])
         setText('')
     }
+    useEffect(() => {
+        fetchTodo()
+    }, [])
+    console.log(todo)
 
     return (
         <div class={style.toDoForm}>
@@ -22,7 +38,7 @@ const toDoForm = () => {
                         }
                         } />
                 </div>
-                <button onClick={addTodo} >Add</button>
+                <button onClick={insertTodo} >Add</button>
                 <div class={style.setList}>
                 {
                     todo.map(t => (<ToDoList text={t} />))
